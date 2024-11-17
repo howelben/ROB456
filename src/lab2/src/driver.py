1162d6228f68a1ae814f707f28ff2150b2ef62a2
howelben <howelben@oregonstate.edu>
2024-11-16 16:30:11 -0800

    Lidar Print

--- src/lab2/src/driver.py
+++ src/lab2/src/driver.py
@@ -138,7 +138,7 @@ class Driver:
 		#  Step 1) Calculate the angle the robot has to turn to in order to point at the target
 		#  Step 2) Set your speed based on how far away you are from the target, as before
 		#  Step 3) Add code that veers left (or right) to avoid an obstacle in front of it
-		
+		print(lidar)
 		theta = atan2(target[1], target[0])
 		print(f"Theta: {theta}")
 		distance = sqrt(target[0] ** 2 + target[1] ** 2)
