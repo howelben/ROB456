14a49cedd90ff5a03b498fbd152a46468b7f1004
howelben <howelben@oregonstate.edu>
2024-12-08 15:20:23 -0800

    SIDnfoaisndg

--- src/lab3/src/student_controller.py
+++ src/lab3/src/student_controller.py
@@ -66,6 +66,7 @@ class StudentController(RobotController):
 		im = np.array(map.data).reshape(map.info.height, map.info.width)
 		im_thresh = pathplan.convert_image(im, 0.3, 0.7)
 		possible_points = explore.find_all_possible_goals(im_thresh)
+		rospy.loginfo(f"Possible points: {possible_points}")
 		robot_pix = explore.convert_x_y_to_pix(im_size, robot_position, size_pix, origin)
 		rospy.loginfo(f"Robot pixel: {robot_pix}")
 		# best_point = explore.find_best_point(im_thresh, possible_points, robot_pix)
@@ -74,7 +75,7 @@ class StudentController(RobotController):
 		# rospy.loginfo(f"Path: {path}")
 		# waypoints = explore.find_waypoints(im_thresh, path)
 		for i in possible_points:
-			rospy.loginfo(possible_points(i))
+			rospy.loginfo(possible_points[i])
 			waypoint  = explore.convert_pix_to_x_y(im_size, possible_points(i), size_pix, origin)
 			rospy.loginfo(waypoint)
 			waypoints_xy + waypoint
