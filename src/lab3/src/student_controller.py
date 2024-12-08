bcea8d3ae12601ec7e6f923b9e8f12c021f37511
howelben <howelben@oregonstate.edu>
2024-12-08 14:29:02 -0800

    Fix robot pixel tuple

--- src/lab3/src/student_controller.py
+++ src/lab3/src/student_controller.py
@@ -67,7 +67,7 @@ class StudentController(RobotController):
 		im_thresh = pathplan.convert_image(im, 0.3, 0.7)
 		possible_points = explore.find_all_possible_goals(im_thresh)
 		robot_pix = explore.convert_x_y_to_pix(im_size, robot_position, size_pix, origin)
-		tuple(robot_pix)
+		robot_pix = tuple(robot_pix)
 		rospy.loginfo(f"Robot pixel: {robot_pix}")
 		best_point = explore.find_best_point(im_thresh, possible_points, robot_pix)
 		rospy.loginfo(f"Best point: {best_point}")
