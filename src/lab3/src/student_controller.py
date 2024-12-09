9d705c029e6ae8b30bee70b7612154b491768470
howelben <howelben@oregonstate.edu>
2024-12-08 17:27:50 -0800

    Waypoint function add

--- src/lab3/src/student_controller.py
+++ src/lab3/src/student_controller.py
@@ -75,10 +75,10 @@ class StudentController(RobotController):
 		rospy.loginfo(f"Best point: {best_point}")
 		path = pathplan.dijkstra(im_thresh, robot_pix, best_point)
 		rospy.loginfo(f"Path: {path}")
-		#waypoints = explore.find_waypoints(im_thresh, path)
-		for point in best_point:
+		waypoints = explore.find_waypoints(im_thresh, path)
+		for point in waypoints:
 			waypoint  = tuple(explore.convert_pix_to_x_y(im_size, point, size_pix, origin))
-			waypoints_xy.append(waypoint)
+			waypoints_xy.append(waypoint[::-1])
 		#waypoints_xy.append(tuple(explore.convert_pix_to_x_y(im_size, list(robot_pix), size_pix, origin)))
 		waypoints_xy = tuple(waypoints_xy)
 		controller.set_waypoints(waypoints_xy)
