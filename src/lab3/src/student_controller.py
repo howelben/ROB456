0b2368d732175adf95b79e87b0ce3be8b252e413
howelben <howelben@oregonstate.edu>
2024-12-10 16:23:37 -0800

    Decrease range

--- src/lab3/src/student_controller.py
+++ src/lab3/src/student_controller.py
@@ -105,7 +105,7 @@ class StudentController(RobotController):
 				for point in possible_points:
 					point_xy = tuple(explore.convert_pix_to_x_y(self.im_size, point, self.size_pix,self.origin))
 					for seen_goal in self.seen_goals:
-						if np.linalg.norm(np.array(seen_goal) - np.array(point_xy)) <= 2.5:
+						if np.linalg.norm(np.array(seen_goal) - np.array(point_xy)) <= 0.5:
 							seen = True
 							break
 					if seen:
